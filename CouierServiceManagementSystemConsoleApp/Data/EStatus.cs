408bf794faedec3faa789ba2cb83dc38dac6ef5e
Mahbub <mahbubalam12890@gmail.com>
2026-09-09 18:19:43 +0600

    add package status as enum

---
+++ CouierServiceManagementSystemConsoleApp/Data/EStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CouierServiceManagementSystemConsoleApp.Data
+{
+    public enum EStatus
+    {
+        Pending,
+        Processing,
+        Shipped,    
+        Cancelled
+    }
+}
