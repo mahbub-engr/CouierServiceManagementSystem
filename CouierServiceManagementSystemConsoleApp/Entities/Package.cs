e232504d6c525124e4c1d2acb94dcabf3116b65d
Mahbub <mahbubalam12890@gmail.com>
2026-09-09 18:20:40 +0600

    little chage

--- CouierServiceManagementSystemConsoleApp/Entities/Package.cs
+++ CouierServiceManagementSystemConsoleApp/Entities/Package.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CouierServiceManagementSystemConsoleApp.Data;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -12,42 +13,16 @@ namespace CouierServiceManagementSystemConsoleApp.Models
 {
     public class Package
     {
-        public string TrackingID { get; set; }
+        public Guid TrackingID { get; set; }
         public string SenderID { get; set; }
         public string ReceiverID { get; set; }
         public double Weight {  get; set; }
-        public string Status { get; set; }
-        private string ConnectionString = "server=.;database =DB_Courier_SMS; integrated security = true";
+        public EStatus Status  { get; set; }
 
 
-        public void AddPackage()
-        {
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
-            {
-                string insertQuery = @"Insert into tbl_package (SenderID,ReceiverID,Weight,Status) values (@SenderID,@ReceiverID,@Weight,@Status) ";
-                using (SqlCommand cmd = new SqlCommand(insertQuery, connection))
-                {
-                    cmd.Parameters.AddWithValue("@SenderID", SenderID);
-                    cmd.Parameters.AddWithValue("@ReceiverID", ReceiverID);
-                    cmd.Parameters.AddWithValue("@Weight", Weight);
-                    cmd.Parameters.AddWithValue("@Status", Status);
-                    connection.Open();
-                    int res = cmd.ExecuteNonQuery();
-                    if (res > 0)
-                    {
-                        Console.WriteLine("Success");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Failed");
-                    }
+        public Customer Sender { get; set; }
+        public Customer Receiver { get; set; }
 
-                }
-
-
-
-            }
-        }
     }
 }
  
