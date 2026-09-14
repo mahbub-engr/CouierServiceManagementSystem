ff6de87093e9f1271fd7ff902f0b7555ba6aac2b
Mahbub <mahbubalam12890@gmail.com>
2026-09-14 21:15:28 +0600

    update courier cs

--- CouierServiceManagementSystemConsoleApp/Entities/Courier.cs
+++ CouierServiceManagementSystemConsoleApp/Entities/Courier.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CouierServiceManagementSystemConsoleApp.Data;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -11,35 +12,9 @@ namespace CouierServiceManagementSystemConsoleApp.Models
 {
     public class Courier
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Status { get; set; }
-
-        private string ConnectionString = "server=.;database =DB_Courier_SMS; integrated security = true";
-        public void AddCourier()
-        {
-
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
-            {
-                string insertQuery = @"Insert into tbl_courier (Name,Status) values (@Name,@Status) ";
-                using (SqlCommand cmd = new SqlCommand(insertQuery, connection))
-                {
-                    cmd.Parameters.AddWithValue("@Name", Name);
-                    cmd.Parameters.AddWithValue("@Status", Status);
-                    connection.Open();
-                    int res = cmd.ExecuteNonQuery();
-                    if (res > 0)
-                    {
-                        Console.WriteLine("Success");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Failed");
-                    }
-
-                }
-
-
-            }
-        }
+        public ECourierStatus Status { get; set; }
+        public string Phone {  get; set; }
     }
 }
